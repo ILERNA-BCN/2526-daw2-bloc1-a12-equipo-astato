bee1caabca23400b9d59fcf7af041ce1875d55c1
LugoDv <falugopersonal@gmail.com>
2025-11-11 19:07:46 +0100

    feat: add logo image and update header with logo integration; enhance styles for header and hero sections

--- login.php
+++ login.php
@@ -19,7 +19,7 @@
 
           <div class="px-5 ms-xl-4">
             <i class="fas fa-crow fa-2x me-3 pt-5 mt-xl-4" style="color: #709085;"></i>
-            <span class="h1 fw-bold mb-0">Logo</span>
+            <span class="h1 fw-bold mb-0"><img src="./assets/images/logo.png" alt="Logo" style="width: 100px; object-fit: cover;" /></span>
           </div>
 
           <div class="d-flex align-items-start h-custom-2 px-5 ms-xl-4 mt-5 pt-5 pt-xl-0 mt-xl-n5 flex-column">
@@ -56,7 +56,7 @@
 
         </div>
         <div class="col-sm-6 px-0 d-none d-sm-block">
-          <img src="./assets/images/login.jpg"
+          <img src="./assets/images/amarillo.svg"
             alt="Login image" class="w-100 vh-100" style="object-fit: cover; object-position: left;">
         </div>
       </div>
